32a15ab4e62b69f36451c9dd29c49eed92da1a12
Captain-ss <swadeshishu2001@gmail.com>
2023-01-22 02:55:27 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Link } from 'react-router-dom';
+import productCard from './Components/productCard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav style={{ background: '#F5F5F5', padding: '20px' }}>
+      <Link to="/" style={{ color: '#000000', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#000000', textDecoration: 'none', marginLeft: '20px' }}>About</Link>
+      <Link to="/contact" style={{ color: '#000000', textDecoration: 'none', marginLeft: '20px' }}>Contact</Link>
+    </nav>
+      <productCard/>
+       
     </div>
   );
 }
