daf2fa8ed113fb0ab233ef1c81560e81d8ce2608
Captain-ss <swadeshishu2001@gmail.com>
2023-01-23 07:58:00 +0530

    OK

--- src/Components/DetailedCard/DetailCard.jsx
+++ src/Components/DetailedCard/DetailCard.jsx
@@ -1,121 +1,81 @@
+import React from "react";
+import "./DetailCard.css";
+import { useState } from "react";
+import Shoes from "../../images/Shoes.jpg";
 
-import React from 'react'
-import "./DetailCard.css"
-import { useState } from 'react';
-import Shoes  from "../../images/Shoes.jpg"
-
-
-
-
-const DetailCard = ({Data}) => {
-    
-  
+const DetailCard = ({ Data }) => {
   return (
     <div
-          className="OurMenu-PopUp-Main-Container
+      className="OurMenu-PopUp-Main-Container
         "
-        >
-        <div
-className="OurMenu-PopUp-box"
->
-<div className="OurMenu-PopUp-box-cross-box">
-  <div>
-    <img src={Shoes} alt="" />
-  </div>
-</div>
-<div className="OurMenu-PopUp-box-div1">
-  
-</div>
-<div className="OurMenu-PopUp-box-div2">
-  {Data.name}
-</div>
-<div className="OurMenu-PopUp-box-div3">
- {Data.description}
-</div>
-<div className="OurMenu-PopUp-box-div4">Make it a Meal</div>
-<div className="OurMenu-PopUp-box-div5">
-  <div className="OurMenu-PopUp-box-div5-part1">
-    <img src={Shoes} alt="" />
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part2">
-    Medium Fries + Chocolate Thick Shake
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part3">
-    <img src={Shoes} alt="" />
-    185
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part4">
-    <input
-      type="radio"
-      name="ItemSize"
-      style={{ width: "1.2vw" }}
-    />
-  </div>
-</div>
-<div className="OurMenu-PopUp-box-div5">
-  <div className="OurMenu-PopUp-box-div5-part1">
-    <img src={Shoes} alt="" />
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part2">
-    Medium Fries + Chocolate Thick Shake
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part3">
-    <img src={Shoes} alt="" />
-    185
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part4">
-    <input
-      type="radio"
-      name="ItemSize"
-      style={{ width: "1.2vw" }}
-    />
-  </div>
-</div>
-<div className="OurMenu-PopUp-box-div5">
-  <div className="OurMenu-PopUp-box-div5-part1">
-    <img src={Shoes} alt="" />
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part2">
-    Medium Fries + Chocolate Thick Shake
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part3">
-    <img src={Shoes} alt="" />
-    185
-  </div>
-  <div className="OurMenu-PopUp-box-div5-part4">
-    <input
-      type="radio"
-      name="ItemSize"
-      style={{ width: "1.2vw" }}
-    />
-  </div>
-</div>
-<div className="OurMenu-PopUp-box-div6">
-  <div className="OurMenu-PopUp-box-div6-part1">
-    <div
-      className="MyCart-Item-Component-div2-text3"
-      style={{ marginLeft: "2vw" }}
     >
-    
-      <span>469</span>
-      <button
-        className="MyCart-Item-Component-div2-text3-button"
-        style={{ marginLeft: "5vw" }}
-      >
-        <div style={{ cursor: "pointer" }}>-</div>
-        <span>1</span>
-        <div1 style={{ cursor: "pointer" }}>+</div1>
-      </button>
-    </div>
-  </div>
-  <div className="OurMenu-PopUp-box-div6-part2">
-    <img src={Shoes} alt="" />
-    add to cart
-  </div>
-</div>
-</div>
-    </div>
-  )
-}
+      <div className="OurMenu-PopUp-box">
+        <div className="OurMenu-PopUp-box-cross-box"></div>
+        <div className="OurMenu-PopUp-box-div1"></div>
+        <div className="OurMenu-PopUp-box-div2">{Data.name}</div>
+        <div className="OurMenu-PopUp-box-div3">{Data.description}</div>
+        {/* <div className="info-boxes-Deatiled-card">
+          <div className="info-box-Deatiled-card">
+            <div>price</div>
+            <div>${Data.price}</div>
+          </div>
+          <div className="info-box-Deatiled-card">
+            <div>Brand</div>
+            <div>{Data.brand}</div>
+          </div>
+          <div className="info-box-Deatiled-card">
+            <div>price</div>
+            <div>${Data.size}</div>
+          </div>
+          <div className="info-box-Deatiled-card">
+            <div>price</div>
+            <div>${Data.price}</div>
+          </div>
+          <div className="info-box-Deatiled-card">
+            <div>price</div>
+            <div>${Data.price}</div>
+          </div>
+        </div> */}
+        <div
+          className="detailedCard-info"
+          style={{
+            display: "flex",
+          }}
+        >
+          Size :{" "}
+          {Array.isArray(Data.size)?
+          Data.size.map((item, idx) => {
+            return <div>{item},</div>;
+          }):Data.size}
+        </div>
+        <div className="detailedCard-info">Price : ${Data.price}</div>
+        <div className="detailedCard-info">Price : ${Data.price}</div>
 
-export default DetailCard
+        <div className="OurMenu-PopUp-box-div6">
+          <div className="OurMenu-PopUp-box-div6-part1">
+            <div
+              className="MyCart-Item-Component-div2-text3"
+              style={{ marginLeft: "2vw" }}
+            >
+              <span>469</span>
+              <button
+                className="MyCart-Item-Component-div2-text3-button"
+                style={{ marginLeft: "5vw" }}
+              >
+                <div style={{ cursor: "pointer" }}>-</div>
+                <span>1</span>
+                <div1 style={{ cursor: "pointer" }}>+</div1>
+              </button>
+            </div>
+          </div>
+          <div className="OurMenu-PopUp-box-div6-part2">
+            <img src={Shoes} alt="" />
+            add to cart
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailCard;
