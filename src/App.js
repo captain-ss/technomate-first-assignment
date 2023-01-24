84feb52e6fe4a8ba52fe94a39d0720c70dce2384
Captain-ss <swadeshishu2001@gmail.com>
2023-01-24 12:32:36 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import ProductCard from "./Components/productCard";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Footer from "./Components/Footer";
-
-
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
