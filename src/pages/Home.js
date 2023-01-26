95631d8f32faf7ec0efc75847307a755e24e4abf
Captain-ss <swadeshishu2001@gmail.com>
2023-01-26 11:13:39 +0530

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,62 @@ import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+import Form from "react-bootstrap/Form";
+
+function AddProductForm() {
+  return (
+    <div className="addProductForm">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name of product</Form.Label>
+          <Form.Control type="text" placeholder="Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Description</Form.Label>
+          <Form.Control type="text" placeholder="Description" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Price of medium size product</Form.Label>
+          <Form.Control type="number" placeholder="235" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Price of large size product</Form.Label>
+          <Form.Control type="number" placeholder="235" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Price of small size product</Form.Label>
+          <Form.Control type="number" placeholder="235" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Color</Form.Label>
+          <Form.Control type="text" placeholder="Color" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Discount in decimal</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="E.g. 0.1,0.3 (discount should not in percent)"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Available city</Form.Label>
+          <Form.Control type="text" placeholder="Delhi ,mumbai,chennai " />
+        </Form.Group>
+        <Button
+          variant="contained"
+          type="submit"
+          style={{
+            border: "1px solid gray",
+          }}
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
 const style = {
   position: "absolute",
   top: "50%",
@@ -22,32 +78,35 @@ const Home = () => {
   return (
     <div>
       <div>
-        <Button onClick={handleOpen}>Add Product</Button>
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={open}>
-            <Box sx={style}>Form goes here</Box>
-          </Fade>
-        </Modal>
-      </div>
-      <div>
-        filter goes here 
+        <div>
+          <Button onClick={handleOpen}>Add Product</Button>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <Box sx={style}>
+                <AddProductForm />
+              </Box>
+            </Fade>
+          </Modal>
+        </div>
+        <div>
+          filters
+        </div>
       </div>
       <div className="Home-product-cards">
         {ProductsData.map((Data, idx) => {
           return <ProductCard Data={Data} key={idx} />;
         })}
       </div>
-
     </div>
   );
 };
